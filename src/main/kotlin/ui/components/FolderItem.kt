309e1d7250bffc4863db1f394cb23c3ed100d2ab
zahid4kh <halilzahid@gmail.com>
2025-10-10 12:52:22 +0200

    added file count if listed item is a directory

--- src/main/kotlin/ui/components/FolderItem.kt
+++ src/main/kotlin/ui/components/FolderItem.kt
@@ -1,18 +1,28 @@
 package ui.components
 
 import MainViewModel
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Folder
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,6 +31,8 @@ import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.File
 
 
@@ -29,34 +41,59 @@ fun FolderItem(
     viewModel: MainViewModel,
     item: String
 ){
+    val scope = rememberCoroutineScope()
+    var count by remember { mutableStateOf(0) }
+    LaunchedEffect(item){
+        scope.launch(Dispatchers.IO) {
+            count = File(item).listFiles().size
+        }
+    }
+
     Row(
-        horizontalArrangement = Arrangement.spacedBy(5.dp),
-        verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
             .clip(MaterialTheme.shapes.small)
-            .pointerInput(Unit){
-                detectTapGestures(
-                    onTap = {  },
-                    onDoubleTap = {  }
-                )
-            }
             .clickable(
                 onClick = { viewModel.setCurrentPath(dir = item) }
-            ).pointerHoverIcon(PointerIcon.Hand)
+            ).pointerHoverIcon(PointerIcon.Hand),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(
-            imageVector = Icons.Default.Folder,
-            contentDescription = null,
-            modifier = Modifier.padding(horizontal = 5.dp, vertical = 1.dp)
-        )
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(5.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.weight(1f)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Folder,
+                contentDescription = null,
+                modifier = Modifier.padding(horizontal = 5.dp, vertical = 1.dp)
+            )
 
-        Text(
-            text = File(item).name,
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier.fillMaxWidth(),
-            overflow = TextOverflow.Ellipsis
-        )
+            Text(
+                text = File(item).name,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.fillMaxWidth(),
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .padding(horizontal = 5.dp)
+                .clip(MaterialTheme.shapes.medium)
+                .background(MaterialTheme.colorScheme.tertiary)
+        ){
+            Text(
+                text = count.toString(),
+                modifier = Modifier.padding(5.dp),
+                color = MaterialTheme.colorScheme.onTertiary
+            )
+        }
+
+
     }
+
 }
 

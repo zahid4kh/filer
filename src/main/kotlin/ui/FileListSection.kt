f2803f41e9f4670e003a4695c2f5598ea85e64bd
zahid4kh <halilzahid@gmail.com>
2025-10-10 20:00:18 +0200

    added vertical scrollbar to file list

--- src/main/kotlin/ui/FileListSection.kt
+++ src/main/kotlin/ui/FileListSection.kt
@@ -1,17 +1,22 @@
 package ui
 
 import MainViewModel
+import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.PointerIcon
 import androidx.compose.ui.input.pointer.pointerHoverIcon
@@ -34,41 +39,51 @@ fun FileListSection(
     LaunchedEffect(uiState.showDotFiles){
         println("Dot files are visible: ${uiState.showDotFiles}")
     }
-    LazyColumn(
-        state = listState,
-        verticalArrangement = Arrangement.spacedBy(3.dp),
-        modifier = Modifier.padding(start = 5.dp, end = 5.dp)
+    Row(
+        modifier = Modifier.fillMaxSize()
     ){
-        item{
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                IconButton(
-                    onClick = { viewModel.handleDotFilesVisibility() },
-                    modifier = Modifier
-                        .pointerHoverIcon(PointerIcon.Hand)
-                ){
-                    Icon(
-                        painter = if(uiState.showDotFiles) painterResource(Res.drawable.eye) else painterResource(Res.drawable.eye_off),
-                        contentDescription = null
+        LazyColumn(
+            state = listState,
+            verticalArrangement = Arrangement.spacedBy(3.dp),
+            modifier = Modifier.padding(start = 5.dp, end = 5.dp).weight(1f)
+        ){
+            item{
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    IconButton(
+                        onClick = { viewModel.handleDotFilesVisibility() },
+                        modifier = Modifier
+                            .pointerHoverIcon(PointerIcon.Hand)
+                    ){
+                        Icon(
+                            painter = if(uiState.showDotFiles) painterResource(Res.drawable.eye) else painterResource(Res.drawable.eye_off),
+                            contentDescription = null
+                        )
+                    }
+                }
+            }
+            items(items = uiState.files.sorted()){ item ->
+                if(File(item).isDirectory){
+                    FolderItem(
+                        viewModel = viewModel,
+                        item = item
+                    )
+                }else{
+                    FileItem(
+                        viewModel = viewModel,
+                        item = item
                     )
                 }
             }
         }
-        items(
-            items = uiState.files.sorted()
-        ){ item ->
-            if(File(item).isDirectory){
-                FolderItem(
-                    viewModel = viewModel,
-                    item = item
-                )
-            }else{
-                FileItem(
-                    viewModel = viewModel,
-                    item = item
-                )
-            }
-        }
+
+        VerticalScrollbar(
+            adapter = rememberScrollbarAdapter(listState),
+            modifier = Modifier
+                .padding(horizontal = 6.dp)
+                .pointerHoverIcon(PointerIcon.Hand)
+        )
     }
+
 }
